b608b0150e19f449e31eb52e13de0f66bf47cd71
Raoul Mendy <raoul.mendy@gmail.com>
2022-06-15 15:21:50 +0200

    Generation of data under JSON format

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,21 @@
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Dashboard from "../Dashboard";
+import data from "../../datas/db.json";
 import { useParams } from "react-router-dom";
 
 function Home() {
+  const { id } = useParams();
+  const [data, setData] = useState([])
 
-  const {id} = useParams();
+
+  // Le useEffect se joue lorsque le composant est monté
+  useEffect(() => {
+    axios.get(data).then((res) => console.log(res.data));
+  }, []);
+
   return (
     <div className="App">
-
       <Dashboard id={id} />
     </div>
   );
